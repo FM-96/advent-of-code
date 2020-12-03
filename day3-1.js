2b359e2612fa87e106bd65c7e47e20f830f429b3
Felix Müllner <felix.muellner.96@gmail.com>
2020-12-03 06:17:06 +0100

    Add day 3 solutions

---
+++ day3-1.js
@@ -0,0 +1,24 @@
+const getInput = require('./getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(3);
+
+	const map = input.split('\n');
+
+	const bottom = map.length - 1;
+
+	let trees = 0;
+	const pos = {x: 0, y: 0};
+	while (pos.y < bottom) {
+		pos.x += 3;
+		pos.y += 1;
+
+		const row = map[pos.y];
+		if (row[pos.x % row.length] === '#') {
+			trees++;
+		}
+	}
+	console.log('Answer: ' + trees);
+}
---
+++ day3-2.js
@@ -0,0 +1,36 @@
+const getInput = require('./getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(3);
+
+	const map = input.split('\n');
+
+	const bottom = map.length - 1;
+
+	const slopes = [
+		{x: 1, y: 1},
+		{x: 3, y: 1},
+		{x: 5, y: 1},
+		{x: 7, y: 1},
+		{x: 1, y: 2},
+	];
+
+	const total = [];
+	for (const slope of slopes) {
+		let trees = 0;
+		const pos = {x: 0, y: 0};
+		while (pos.y < bottom) {
+			pos.x += slope.x;
+			pos.y += slope.y;
+
+			const row = map[pos.y];
+			if (row[pos.x % row.length] === '#') {
+				trees++;
+			}
+		}
+		total.push(trees);
+	}
+	console.log('Answer: ' + total.reduce((acc, cur) => acc * cur));
+}
