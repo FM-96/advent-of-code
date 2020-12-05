65e124cbc60d8d6faacc85346fcd470cae6a3ae0
Felix Müllner <felix.muellner.96@gmail.com>
2020-12-05 06:39:17 +0100

    Add day 5 solutions

---
+++ day5-2.js
@@ -0,0 +1,42 @@
+const getInput = require('./getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(5);
+
+	const passes = input.split('\n');
+
+	let highestId = 0;
+	const allIds = new Set();
+	for (const pass of passes) {
+		const row = binaryPartitioning(pass.split('').slice(0, 7).map(e => e === 'B'));
+		const col = binaryPartitioning(pass.split('').slice(7).map(e => e === 'R'));
+
+		const seatId = (row * 8) + col;
+		allIds.add(seatId);
+		highestId = Math.max(highestId, seatId);
+	}
+
+	for (let i = highestId - 1; i >= 0; --i) {
+		if (!allIds.has(i)) {
+			console.log('Answer: ' + i);
+			return;
+		}
+	}
+}
+
+function binaryPartitioning(instructions) {
+	let min = 0;
+	let max = (2 ** instructions.length) - 1;
+
+	for (const instruction of instructions) {
+		const range = max - min + 1;
+		if (instruction) { // true === upper half
+			min += range / 2;
+		} else { // false === lower half
+			max -= range / 2;
+		}
+	}
+	return min;
+}
