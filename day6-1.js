b5eab4def053e548600a71edb1329b5aa76be2bb
Felix Müllner <felix.muellner.96@gmail.com>
2020-12-06 06:21:19 +0100

    Add day 6 solutions

---
+++ day6-1.js
@@ -0,0 +1,23 @@
+const getInput = require('./getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(6);
+
+	const groups = input.split('\n\n');
+
+	let sum = 0;
+	for (const group of groups) {
+		const yesQuestions = new Set();
+
+		const personQuestions = group.replace(/\n/g, '').split('');
+
+		for (const personQuestion of personQuestions) {
+			yesQuestions.add(personQuestion);
+		}
+		sum += yesQuestions.size;
+	}
+
+	console.log('Answer: ' + sum);
+}
---
+++ day6-2.js
@@ -0,0 +1,34 @@
+const getInput = require('./getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(6);
+
+	const groups = input.split('\n\n');
+
+	let sum = 0;
+	for (const group of groups) {
+		const yesQuestions = new Set();
+
+		const persons = group.split('\n');
+
+		for (let i = 0; i < persons.length; ++i) {
+			const personQuestions = persons[i].split('');
+			if (i === 0) {
+				for (const personQuestion of personQuestions) {
+					yesQuestions.add(personQuestion);
+				}
+			} else {
+				for (const value of yesQuestions.values()) {
+					if (!personQuestions.includes(value)) {
+						yesQuestions.delete(value);
+					}
+				}
+			}
+		}
+		sum += yesQuestions.size;
+	}
+
+	console.log('Answer: ' + sum);
+}
