67b676c607cf2077398f890190abab387e5c1695
Felix Müllner <felix.muellner.96@gmail.com>
2020-12-13 07:01:13 +0100

    Add day 13 part 1 solution

---
+++ day13-1.js
@@ -0,0 +1,24 @@
+const getInput = require('./getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(13);
+
+	let [earliestDeparture, schedule] = input.split('\n');
+	earliestDeparture = Number(earliestDeparture);
+
+	const busIds = schedule.split(',').filter(e => e !== 'x').map(e => Number(e));
+
+	let time = earliestDeparture;
+	for (;;) {
+		const bus = busIds.find(e => time % e === 0);
+		if (!bus) {
+			time++;
+			continue;
+		}
+
+		console.log('Answer: ' + ((time - earliestDeparture) * bus));
+		break;
+	}
+}
