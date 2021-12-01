45f691026b6ff74c042125ffd9bc211690a0190f
Felix Müllner <felix.muellner.96@gmail.com>
2021-12-01 19:14:45 +0100

    Add day 1 solutions

---
+++ 2021/day1-2.js
@@ -0,0 +1,17 @@
+const getInput = require('../getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(1);
+
+	const measurements = input.split('\n').map(e => Number(e));
+
+	let increases = 0;
+	for (let i = 3; i < measurements.length; ++i) {
+		if ((measurements[i] + measurements[i - 1] + measurements[i - 2]) > (measurements[i - 1] + measurements[i - 2] + measurements[i - 3])) {
+			increases++;
+		}
+	}
+	console.log('Answer: ' + increases);
+}
