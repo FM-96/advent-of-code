610d0091d55920aefa39ae617e8bf3620519db88
Felix Müllner <felix.muellner.96@gmail.com>
2021-12-04 12:58:50 +0100

    Add day 3 solutions

---
+++ 2021/day3-1.js
@@ -0,0 +1,27 @@
+const getInput = require('../getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(3);
+
+	const numbers = input.split('\n').map(e => e.trim());
+
+	let gammaRate = '';
+	for (let i = 0; i < numbers[0].length; ++i) {
+		let ones = 0;
+		let zeroes = 0;
+		for (const number of numbers) {
+			if (number[i] === '1') {
+				ones++;
+			}
+			if (number[i] === '0') {
+				zeroes++;
+			}
+		}
+		gammaRate += (ones > zeroes) ? '1' : '0';
+	}
+	const epsilonRate = gammaRate.split('').map(e => (e === '0' ? '1' : '0')).join('');
+
+	console.log('Answer: ' + (parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)));
+}
---
+++ 2021/day3-2.js
@@ -0,0 +1,44 @@
+const getInput = require('../getInput.js');
+
+main();
+
+async function main() {
+	const input = await getInput(3);
+
+	const numbers = input.split('\n').map(e => e.trim());
+
+	let ogRating = numbers.slice();
+	let csRating = numbers.slice();
+
+	for (let i = 0; i < numbers[0].length; ++i) {
+		if (ogRating.length > 1) {
+			const ogBitCriteria = getBitCriteria(ogRating, i, 'most');
+			ogRating = ogRating.filter(e => e[i] === ogBitCriteria);
+		}
+		if (csRating.length > 1) {
+			const csBitCriteria = getBitCriteria(csRating, i, 'least');
+			csRating = csRating.filter(e => e[i] === csBitCriteria);
+		}
+	}
+
+	console.log('Answer: ' + (parseInt(ogRating[0], 2) * parseInt(csRating[0], 2)));
+}
+
+function getBitCriteria(arr, i, criteria) {
+	let ones = 0;
+	let zeroes = 0;
+	for (const number of arr) {
+		if (number[i] === '1') {
+			ones++;
+		}
+		if (number[i] === '0') {
+			zeroes++;
+		}
+	}
+	if (criteria === 'most') {
+		return (ones >= zeroes) ? '1' : '0';
+	}
+	if (criteria === 'least') {
+		return (ones < zeroes) ? '1' : '0';
+	}
+}
